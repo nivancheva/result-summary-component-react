3b04a71fc38f847137493d0b613dfd71306e83c1
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-09-25 14:03:24 +0300

    add general styling

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className='result-summary'>
         <div className='results'>
           <h1>Your result</h1>
-          <p><span>76</span> of 100</p>
+          <p className='result-score'><span>76</span> of 100</p>
           <div>
             <p>Great</p>
             <p>You scored higher than 65% of the people who have taken these tests.</p>
