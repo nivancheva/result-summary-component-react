f1a6053e681840e18ef9766d9e5c516fc071050e
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-09-25 13:22:30 +0300

    add SummaryItem.css file

--- src/SummaryItem.js
+++ src/SummaryItem.js
@@ -1,6 +1,8 @@
+import './SummaryItem.css';
+
 export default function SummaryItem({icon, category, score, color}) {
     return (
-        <div className={"summary-item item-" + color}>   
+        <div className={"flex-group summary-item item-" + color}>   
             <div className="flex-group">
                 <img src={icon} alt={category} />
                 <h3>{category}</h3>
